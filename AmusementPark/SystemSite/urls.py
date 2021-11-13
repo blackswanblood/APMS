987556a998885d7c378ce50a9eec039221f096eb
Apricot_HT <haotiangong@hotmail.com>
2021-11-12 17:19:33 -0800

    added viewalltourists.html

--- AmusementPark/SystemSite/urls.py
+++ AmusementPark/SystemSite/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
 from . import views
-
 from django.conf.urls import url
 from .views import index
 
 
 urlpatterns =[
-    url(r'^$', index, name = "index"),
+    url(r'^$', views.index, name = "index"),
+    url(r'^viewalltourists/$', views.viewAllTourists),
 ]
--- AmusementPark/SystemSite/views.py
+++ AmusementPark/SystemSite/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
-
-from django.shortcuts import render
+from .models import Tourist
 
 def index (request):
     return render(request, 'SystemSite/index.html')
+
+def viewAllTourists(request):
+    tourist_list = Tourist.objects.raw('SELECT * FROM Tourist')
+    template = loader.get_template('SystemSite/viewalltourists.html')
+    context = {
+        'tourist_list': tourist_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
