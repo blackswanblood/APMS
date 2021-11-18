998996828ba734c4d9ecf6f46f6d8a6f1edc8e3d
luckyberen <sunweihao1226@gmail.com>
2021-11-17 20:01:06 -0800

    Modify html template, add tables.

--- AmusementPark/SystemSite/views.py
+++ AmusementPark/SystemSite/views.py
@@ -2,10 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Tourist
+from .models import Tourist, Staff
 
 def index (request):
-    return render(request, 'SystemSite/index.html')
+    tourist_list = Tourist.objects.raw('SELECT * FROM Tourist')
+    staff_list = Staff.objects.raw ('SELECT * FROM Staff')
+
+    tourist_count = len(list(tourist_list))
+    staff_count = len(list(staff_list))
+
+    template = loader.get_template('SystemSite/index.html')
+    context = {
+        'tourist_list': tourist_list,
+        'tourist_count': tourist_count,
+        'staff_count' : staff_count
+    }
+    return HttpResponse(template.render(context, request))
+    # return render(request, 'SystemSite/index.html')
 
 def viewAllTourists(request):
     tourist_list = Tourist.objects.raw('SELECT * FROM Tourist')
